e9cf1f03bd35d524ec1fd2e69655c2d709f3038a
sajadsabri <sajadsabri@users.noreply.github.com>
2024-09-24 08:33:50 +0000

    deploy: 9c5d255d77774c915993e6fb39028eb160d2bda8

--- fa/assets/js/deb94cdb.0af8a982.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkuimotelndefined=self.webpackChunkuimotelndefined||[]).push([[6920],{9970:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/fa/intro-softphone","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
