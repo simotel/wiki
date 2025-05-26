43e1c11680808f2b87f630135a939054877b72c3
hoseinalizadeh1373 <hoseinalizadeh1373@users.noreply.github.com>
2025-05-26 09:18:46 +0000

    deploy: 0d3913a01ac69224c8773e708a6ce44d7607d4dd

---
+++ fa/assets/js/5e95c892.a55b4231.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkuimotelndefined=self.webpackChunkuimotelndefined||[]).push([[9647],{8122:(e,s,n)=>{n.r(s),n.d(s,{default:()=>i});n(6540);var r=n(8215),u=n(1769),d=n(204),t=n(2831),a=n(9864),c=n(4848);function i(e){return(0,c.jsx)(u.e3,{className:(0,r.A)(d.G.wrapper.docsPages),children:(0,c.jsx)(a.A,{children:(0,t.v)(e.route.routes)})})}}}]);
